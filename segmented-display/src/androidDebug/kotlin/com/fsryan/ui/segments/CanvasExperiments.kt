887eebd0c209bc4953a9152aab5054cdecbc1e93
Ryan <ryansgot@gmail.com>
2024-07-18 00:27:18 +0900

    014_render_a_canvas_in_preview

--- segmented-display/src/androidDebug/kotlin/com/fsryan/ui/segments/CanvasExperiments.kt
+++ segmented-display/src/androidDebug/kotlin/com/fsryan/ui/segments/CanvasExperiments.kt
@@ -1,2 +1,22 @@
 package com.fsryan.ui.segments
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+fun RenderACanvas() {
+    Canvas(
+        modifier = Modifier.width(50.dp).height(100.dp)
+            .background(Color.Red)
+    ) {
+
+    }
+}
