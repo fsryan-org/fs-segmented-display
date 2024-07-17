b199bfbc169d60fca4a8a1c04b044aec33e41c57
Ryan <ryansgot@gmail.com>
2024-07-18 03:07:32 +0900

    019_initial_draw_rect_7_segment_char

--- segmented-display/src/androidDebug/kotlin/com/fsryan/ui/segments/Rect7SegmentDisplayPreview.kt
+++ segmented-display/src/androidDebug/kotlin/com/fsryan/ui/segments/Rect7SegmentDisplayPreview.kt
@@ -1,2 +1,23 @@
 package com.fsryan.ui.segments
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+fun DrawRect7SegmentChar() {
+    Canvas(
+        modifier = Modifier.width(200.dp)
+            .height(400.dp)
+    ) {
+        drawRect7SegmentChar(
+            width = size.width,
+            height = size.height
+        )
+    }
+}
--- segmented-display/src/commonMain/kotlin/com/fsryan/ui/segments/Rect7SegmentDisplay.kt
+++ segmented-display/src/commonMain/kotlin/com/fsryan/ui/segments/Rect7SegmentDisplay.kt
@@ -1,2 +1,9 @@
 package com.fsryan.ui.segments
 
+import androidx.compose.ui.graphics.drawscope.DrawScope
+
+fun DrawScope.drawRect7SegmentChar(
+    width: Float,
+    height: Float
+) {
+}
