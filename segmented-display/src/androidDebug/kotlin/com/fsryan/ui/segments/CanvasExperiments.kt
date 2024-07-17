80f8b806f2e0ba700965e71e12f1ad675f812273
Ryan <ryansgot@gmail.com>
2024-07-18 00:47:27 +0900

    015_vertical_and_horizontal_line

--- segmented-display/src/androidDebug/kotlin/com/fsryan/ui/segments/CanvasExperiments.kt
+++ segmented-display/src/androidDebug/kotlin/com/fsryan/ui/segments/CanvasExperiments.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -18,5 +19,33 @@ fun RenderACanvas() {
             .background(Color.Red)
     ) {
 
+    }
+}
+
+@Preview
+@Composable
+fun RenderAVerticalLine() {
+    Canvas(
+        modifier = Modifier.width(50.dp).height(100.dp)
+    ) {
+        drawLine(
+            color = Color.Blue,
+            start = Offset(x = size.width / 2, y = 0F),
+            end = Offset(x = size.width / 2, y = size.height)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun RenderAHorizontalLine() {
+    Canvas(
+        modifier = Modifier.width(50.dp).height(100.dp)
+    ) {
+        drawLine(
+            color = Color.Green,
+            start = Offset(x = 0F, y = size.height / 2),
+            end = Offset(x = size.width, y = size.height / 2)
+        )
     }
 }
