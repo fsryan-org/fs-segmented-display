f97d37e6b4812e944a5034b834d10a36acce6ab7
Ryan <ryansgot@gmail.com>
2024-07-18 15:26:25 +0900

    028_demonstrate_rectangular_7_segments_in_composeApp

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,7 +1,11 @@
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -9,7 +13,8 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import com.fsryan.ui.SampleComposable
+import androidx.compose.ui.unit.dp
+import com.fsryan.ui.segments.Rect7SegmentDisplay
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -26,11 +31,28 @@ fun App() {
                 Text("Click me!")
             }
             AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    SampleComposable(Color.Blue)
-                    Text("Compose: $greeting")
+                BoxWithConstraints {
+                    val width = maxWidth
+                    Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                        Rect7SegmentDisplay(
+                            modifier = Modifier.background(Color.LightGray)
+                                .fillMaxWidth()
+                                .height(width / 4),
+                            text = "01234567",
+                            thicknessMultiplier = 0.6F,
+                            gapSizeMultiplier = 0.75F,
+                            activatedColor = Color.Red
+                        )
+                        Rect7SegmentDisplay(
+                            modifier = Modifier.background(Color.LightGray)
+                                .fillMaxWidth()
+                                .height(width / 4),
+                            thicknessMultiplier = 0.6F,
+                            gapSizeMultiplier = 0.75F,
+                            text = "89ABCDEF",
+                            activatedColor = Color.Red
+                        )
+                    }
                 }
             }
         }
